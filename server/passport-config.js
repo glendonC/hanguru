1d262ce615ad086f5d40c4a81834c0e33a20ed61
Glendon Chin <glendonchin@gmail.com>
2023-11-02 10:00:16 -0400

    Set up basic password configuration

---
+++ server/passport-config.js
@@ -0,0 +1,31 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+
+function initialize(passport) {
+  const authenticateUser = async (email, password, done) => {
+    // Match user by email
+    const user = await User.findOne({ email: email });
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Password incorrect' });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser(async (id, done) => {
+    return done(null, await User.findById(id));
+  });
+}
+
+module.exports = initialize;
