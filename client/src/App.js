03aac4c07fc0b1b8b7a45bb55e4daebec3a30cbe
glendonchin <glendonchin@gmail.com>
2023-11-15 08:46:12 -0500

    Set up basic login and home page with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <LoginPage onLogin={() => setLoggedIn(true)} />} />
+        <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
