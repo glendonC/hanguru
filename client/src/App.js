6d48423c458ab95d2a169b479cecf725c8d3d5c0
glendonchin <glendonchin@gmail.com>
2023-11-16 14:17:28 -0500

    Implement Navigation Bar structure and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import VocabularyPage from './pages/VocabularyPage';
+import ExercisesPage from './pages/ExercisesPage';
+import AudioRecordingPage from './pages/AudioRecordingPage';
 
 function App() {
   const [isLoggedIn, setLoggedIn] = useState(false);
@@ -17,6 +21,7 @@ function App() {
 
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route 
           path="/auth" 
@@ -25,6 +30,20 @@ function App() {
         <Route 
           path="/" 
           element={isLoggedIn ? <HomePage /> : <Navigate to="/auth" />} 
+        />
+
+        {/* New Routes */}
+        <Route 
+          path="/vocabulary" 
+          element={isLoggedIn ? <VocabularyPage /> : <Navigate to="/auth" />} 
+        />
+        <Route 
+          path="/exercises" 
+          element={isLoggedIn ? <ExercisesPage /> : <Navigate to="/auth" />} 
+        />
+        <Route 
+          path="/audio-recording" 
+          element={isLoggedIn ? <AudioRecordingPage /> : <Navigate to="/auth" />} 
         />
       </Routes>
     </Router>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
+
+// Define the links for your specific features
+const navLinks = [
+  { name: 'Vocabulary', path: '/vocabulary' },
+  { name: 'Exercises', path: '/exercises' },
+  { name: 'Audio Recording', path: '/audio-recording' }
+];
+
+const NavLink = ({ children, to }) => (
+  <Link
+    to={to}
+    style={{
+      padding: '8px 16px',
+      borderRadius: '4px',
+      textDecoration: 'none',
+      backgroundColor: useColorModeValue('gray.200', 'gray.700')
+    }}>
+    {children}
+  </Link>
+);
+
+export default function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>Logo</Box>
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {navLinks.map((link) => (
+                <NavLink key={link.name} to={link.path}>{link.name}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
+            {/* User Menu */}
+            {/* ... */}
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {navLinks.map((link) => (
+                <NavLink key={link.name} to={link.path}>{link.name}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
