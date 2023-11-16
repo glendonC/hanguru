f93999d9929a534efe375265ea5ff36cd5bca1e6
glendonchin <glendonchin@gmail.com>
2023-11-16 14:58:38 -0500

    Add filler code

---
+++ client/src/pages/AudioRecordingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AudioRecordingPage = () => {
+  return (
+    <div>
+      <h1>Audio Recording Page</h1>
+      {}
+    </div>
+  );
+}
+
+export default AudioRecordingPage;
---
+++ client/src/pages/ExercisesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExercisesPage = () => {
+  return (
+    <div>
+      <h1>Exercises Page</h1>
+      {}
+    </div>
+  );
+}
+
+export default ExercisesPage;
