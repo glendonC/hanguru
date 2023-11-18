a706455b6df6dbd2b126539453a2e72cc754f2e9
Glendon Chin <glendonchin@gmail.com>
2023-11-18 16:45:20 -0500

    Implement exercise functionality using sentences

--- client/src/pages/ExercisesPage.js
+++ client/src/pages/ExercisesPage.js
@@ -1,23 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Box, VStack, Select, Button, Text, CheckboxGroup, Checkbox, Stack, Input } from '@chakra-ui/react';
+import { Box, VStack, Select, Button, Text, Checkbox, Input } from '@chakra-ui/react';
 import axios from 'axios';
 
 const ExercisesPage = () => {
   const [vocabularySets, setVocabularySets] = useState([]);
-  const [currentSet, setCurrentSet] = useState('');
-  const [selectedWord, setSelectedWord] = useState('');
-  const [generatedSentence, setGeneratedSentence] = useState('');
-  const [translatedSentence, setTranslatedSentence] = useState('');
   const [setWords, setSetWords] = useState([]);
   const [selectedWords, setSelectedWords] = useState([]);
-
-  const [grammarPoint, setGrammarPoint] = useState('');
-    const [correctAnswer, setCorrectAnswer] = useState('');
-    const [userInput, setUserInput] = useState('');
-    const [feedback, setFeedback] = useState('');
-
-
-
+  const [userInput, setUserInput] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const [generatedQuestion, setGeneratedQuestion] = useState('');
+  const [questionTranslation, setQuestionTranslation] = useState('');
 
   useEffect(() => {
     const fetchSets = async () => {
@@ -32,12 +24,7 @@ const ExercisesPage = () => {
   }, []);
 
   const handleSetSelection = async (setId) => {
-    setSelectedWord('');
-    setGeneratedSentence('');
-    setTranslatedSentence('');
     setSelectedWords([]);
-  
-
     try {
       const response = await axios.get(`http://localhost:8100/api/vocabulary/set/${setId}/items`);
       setSetWords(response.data);
@@ -47,102 +34,68 @@ const ExercisesPage = () => {
   };
 
   const handleWordSelection = (word) => {
-    setSelectedWords((prevSelectedWords) =>
-      prevSelectedWords.includes(word)
-        ? prevSelectedWords.filter(w => w !== word)
-        : [...prevSelectedWords, word]
+    setSelectedWords(prevSelectedWords =>
+      prevSelectedWords.includes(word) ? prevSelectedWords.filter(w => w !== word) : [...prevSelectedWords, word]
     );
   };
 
-  const generateSentence = async () => {
+  const generateQuestion = async () => {
     try {
       const response = await axios.post('http://localhost:8100/api/generate-sentence', {
-          vocab: selectedWords,
+        vocab: selectedWords.join(', '),
       });
-
-      setGeneratedSentence(response.data.sentenceWithBlank);
-      setCorrectAnswer(response.data.correctAnswer);
-      setTranslatedSentence(response.data.translation);
-      setGrammarPoint(response.data.grammarPoint);
-  } catch (error) {
-      console.error('Error generating sentence', error);
-  }
-};
-
-
-// Function to check user's input
-const checkAnswer = () => {
-  if (userInput.trim() === correctAnswer.trim()) {
-      setFeedback('Correct! Well done.');
-  } else {
-      setFeedback('Incorrect. Try again.');
-  }
-};
-
-  const translateSentence = async () => {
-    try {
-      const response = await axios.post('http://localhost:8100/api/translate', {
-        text: generatedSentence,
-        sourceLang: 'ko',
-        targetLang: 'en'
-      });
-
-      const translation = response.data.translation;
-      setTranslatedSentence(translation);
+      setGeneratedQuestion(response.data.question || 'No question provided');
+      setQuestionTranslation(response.data.translation || 'No translation available');
     } catch (error) {
-      console.error('Error translating sentence:', error);
+      console.error('Error generating question', error);
+    }
+  };
+
+  const checkUserAnswer = async () => {
+    try {
+      const response = await axios.post('http://localhost:8100/api/check-answer', {
+        userSentence: userInput,
+        question: generatedQuestion,
+        vocab: selectedWords.join(', ')
+      });
+      setFeedback(response.data.feedback);
+    } catch (error) {
+      console.error('Error checking answer:', error);
+      setFeedback('Error checking answer.');
     }
   };
 
   return (
     <Box p={5}>
-        <VStack spacing={4}>
-            {/* Vocabulary set selection */}
-            <Select placeholder="Select set" onChange={(e) => handleSetSelection(e.target.value)}>
-                {vocabularySets.map((set) => (
-                    <option key={set._id} value={set._id}>{set.setName}</option>
-                ))}
-            </Select>
+      <VStack spacing={4}>
+        <Select placeholder="Select set" onChange={e => handleSetSelection(e.target.value)}>
+          {vocabularySets.map(set => (
+            <option key={set._id} value={set._id}>{set.setName}</option>
+          ))}
+        </Select>
 
-            {/* Words selection */}
-            {setWords.map((word) => (
-                <Checkbox 
-                    key={word._id} 
-                    isChecked={selectedWords.includes(word.korean)}
-                    onChange={() => handleWordSelection(word.korean)}
-                >
-                    {word.korean}
-                </Checkbox>
-            ))}
+        {setWords.map(word => (
+          <Checkbox key={word._id} isChecked={selectedWords.includes(word.korean)} onChange={() => handleWordSelection(word.korean)}>
+            {word.korean}
+          </Checkbox>
+        ))}
 
-            {/* Generate sentence button */}
-            <Button colorScheme="blue" onClick={generateSentence} disabled={selectedWords.length === 0}>
-                Generate Sentence
-            </Button>
+        <Button colorScheme="blue" onClick={generateQuestion} disabled={selectedWords.length === 0}>
+          Generate Question
+        </Button>
 
-            {/* Display generated sentence with a blank */}
-            {generatedSentence && (
-                <>
-                    <Text mt={2}>Fill in the blank (Korean): {generatedSentence}</Text>
-                    <Input 
-                        placeholder="Type your answer here" 
-                        value={userInput} 
-                        onChange={(e) => setUserInput(e.target.value)}
-                    />
-                    <Button mt={2} colorScheme="teal" onClick={checkAnswer}>Check Answer</Button>
-                    <Button mt={2} colorScheme="orange" onClick={() => setFeedback(`The correct answer is: ${correctAnswer}`)}>See Answer</Button>
-                    {feedback && <Text mt={2}>{feedback}</Text>}
-                </>
-            )}
-
-{translatedSentence && <Text mt={2}>Translation (English): {translatedSentence}</Text>}
-            {grammarPoint && <Text mt={2}>Grammar Point: {grammarPoint}</Text>}
-        </VStack>
+        {generatedQuestion && (
+          <>
+            <Text mt={2}>Question (Korean): {generatedQuestion}</Text>
+            <Text mt={2}>Translation (English): {questionTranslation}</Text>
+            <Input placeholder="Type your answer here" value={userInput} onChange={e => setUserInput(e.target.value)} />
+            <Button mt={2} colorScheme="teal" onClick={checkUserAnswer}>Get Feedback</Button>
+            {feedback && <Text mt={2}>{feedback}</Text>}
+          </>
+        )}
+      </VStack>
     </Box>
-);
-
-
-
+  );
 }
 
 export default ExercisesPage;
--- server/routes/gpt.js
+++ server/routes/gpt.js
@@ -4,8 +4,7 @@ const axios = require('axios');
 
 router.post('/generate-sentence', async (req, res) => {
     const { vocab } = req.body;
-    const prompt = `Create a sentence in Korean using the word '${vocab}' and apply the appropriate grammar conjugation. Then, provide the same sentence with a blank in place of the conjugated '${vocab}', the correct conjugated form of '${vocab}' as the answer, the grammar point used for this conjugation, and the English translation of the sentence.`;
-
+    const prompt = `Create a question in Korean that prompts someone to write a sentence using the word '${vocab}'. Then provide its English translation in the following format: 'Korean Question: [Korean question] English Translation: [English translation].'`;
     try {
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
             model: "gpt-3.5-turbo",
@@ -17,29 +16,46 @@ router.post('/generate-sentence', async (req, res) => {
             }
         });
 
-        const gptResponse = response.data.choices[0].message.content; 
+        const gptResponse = response.data.choices[0].message.content;
         console.log("GPT Response:", gptResponse);
         
-        const lines = gptResponse.split('\n').map(line => line.trim());
-        const sentenceWithBlankIndex = lines.findIndex(line => line.startsWith('Blank:'));
-        const correctAnswerIndex = lines.findIndex(line => line.startsWith('Answer:'));
-        const grammarPointIndex = lines.findIndex(line => line.includes('Grammar point:'));
-        const translationIndex = lines.findIndex(line => line.startsWith('English translation:'));
+        let question = gptResponse.match(/Korean Question:\s*([^\n]+)/)?.[1].trim() || 'No question provided';
+        let translation = gptResponse.match(/English Translation:\s*([^\n]+)/)?.[1].trim() || 'No translation available';
         
-        const sentenceWithBlank = sentenceWithBlankIndex !== -1 ? lines[sentenceWithBlankIndex].split('Blank: ')[1] : 'No sentence with blank provided';
-        const correctAnswer = correctAnswerIndex !== -1 ? lines[correctAnswerIndex].split('Answer: ')[1] : 'No correct answer provided';
-        const grammarPoint = grammarPointIndex !== -1 ? lines[grammarPointIndex].split('Grammar point: ')[1] : 'No grammar point provided';
-        const translation = translationIndex !== -1 ? lines[translationIndex].split('English translation: ')[1] : 'No translation available';
-        
+        console.log("Sending response:", { question, translation });
+
         res.json({
-            sentenceWithBlank: sentenceWithBlank,
-            correctAnswer: correctAnswer,
-            grammarPoint: grammarPoint,
+            question: question,
             translation: translation
         });
     } catch (error) {
         console.error('Error in OpenAI request:', error);
-        res.status(500).send('Error generating sentence');
+        res.status(500).send('Error generating question');
+    }
+});
+
+router.post('/check-answer', async (req, res) => {
+    const { userSentence, question, vocab } = req.body;
+    const prompt = `Check if the sentence '${userSentence}' correctly and naturally answers the question '${question}' and uses the word '${vocab}' appropriately. Provide feedback on the correctness and naturalness.`;
+
+    try {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "system", content: "You are a helpful assistant." }, { role: "user", content: prompt }],
+            max_tokens: 100
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
+
+        const feedback = response.data.choices[0].message.content;
+        console.log("GPT Feedback:", feedback);
+
+        res.json({ feedback });
+    } catch (error) {
+        console.error('Error in OpenAI request:', error);
+        res.status(500).send('Error checking answer');
     }
 });
 
