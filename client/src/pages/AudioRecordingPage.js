67467b447c9ce69b78dd45b9a3c6dc6dc462a83a
Glendon Chin <glendonchin@gmail.com>
2023-11-18 21:26:03 -0500

    Set up Audio Recording frontend page

--- client/src/pages/AudioRecordingPage.js
+++ client/src/pages/AudioRecordingPage.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AudioRecordingPage = () => {
+  const [recording, setRecording] = useState(false);
+  const [audioURL, setAudioURL] = useState('');
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const recorder = new MediaRecorder(stream);
+    let audioChunks = [];
+
+    recorder.ondataavailable = event => {
+      audioChunks.push(event.data);
+    };
+
+    recorder.onstop = async () => {
+      const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+      const url = URL.createObjectURL(audioBlob);
+      setAudioURL(url);
+      audioChunks = [];
+
+      // Here, you would upload the Blob to the server
+      uploadAudio(audioBlob);
+    };
+
+    recorder.start();
+    setMediaRecorder(recorder);
+    setRecording(true);
+  };
+
+  const stopRecording = () => {
+    mediaRecorder.stop();
+    setRecording(false);
+  };
+
+  const uploadAudio = async (audioBlob) => {
+    const formData = new FormData();
+    formData.append('file', audioBlob);
+
+    const response = await fetch('http://localhost:8100/upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // Handle success - maybe display a success message to the user
+      console.log('Upload successful:', data.message);
+    } else {
+      // Handle error - inform the user that the upload failed
+      console.error('Upload failed');
+    }
+  };
+
   return (
     <div>
-      <h1>Audio Recording Page</h1>
-      {}
+      <button onClick={recording ? stopRecording : startRecording}>
+        {recording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      {audioURL && <audio src={audioURL} controls />}
     </div>
   );
-}
+};
 
 export default AudioRecordingPage;
