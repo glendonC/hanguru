61b45827a5d07ddd5251e950139217c8c5081c3f
Glendon Chin <glendonchin@gmail.com>
2023-11-22 05:33:23 -0500

    Update util documentation

--- server/utils/googleCloudStorage.js
+++ server/utils/googleCloudStorage.js
@@ -3,6 +3,14 @@ const { Storage } = require('@google-cloud/storage');
 const storage = new Storage({ keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS });
 const bucketName = process.env.GCS_BUCKET_NAME;
 
+/**
+ * Asynchronously uploads a file to Google Cloud Storage.
+ *
+ * The function takes a file object (typically from a multer upload) and uploads it
+ * to the specified Google Cloud Storage bucket (hanguru audio bucket). The uploaded file is stored in a directory
+ * named 'uploads' within the bucket
+ *
+ */
 async function uploadToGoogleCloud(file) {
   await storage.bucket(bucketName).upload(file.path, {
     destination: `uploads/${file.filename}`,
--- server/utils/textToSpeech.js
+++ server/utils/textToSpeech.js
@@ -2,6 +2,16 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 
 const client = new textToSpeech.TextToSpeechClient();
 
+/**
+ * Asynchronously generates speech from text using Google Cloud's Text-to-Speech API.
+ *
+ * @param {string} text - The text to be converted into speech
+ * @param {string} voiceName - The name of the voice to be used for the speech synthesis
+ *   The voice name should correspond to one of the available voices in Google Cloud Text-to-Speech in KOR
+ * 
+ * @returns {Promise<Buffer>} - A promise that resolves to the buffer containing the MP3 audio content.
+ * 
+ */
 async function generateSpeech(text, voiceName) {
   const request = {
     input: { text: text },
