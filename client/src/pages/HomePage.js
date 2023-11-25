46e5d21aa8b6f497a83b05adf921381bf09233c4
Glendon Chin <glendonchin@gmail.com>
2023-11-25 14:37:26 -0500

    Update home page styling with animation and titles

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,84 +1,89 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Button,
-  Input,
-  FormControl,
-  FormLabel,
-  Textarea,
-  VStack,
-  useToast
-} from '@chakra-ui/react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Box, Heading, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 /**
  * HomePage Component
  * 
- * This component serves as the home page of the application and includes functionality
- * for translating text from English to Korean
+ * This component serves as the home page of Hanguru
  * 
  * State Management:
- * - textToTranslate: Stores the text input by the user that needs to be translated
- * - translation: Stores the translated text received from the server
+ * - showEnglish: Toggles between the English and Korean versions of the welcome message
+ * - mottoIndex: Controls which motto is currently displayed
  * 
  * Features:
- * - Text input for the user to enter text in English
- * - Button to initiate the translation process
- * - Display area for the translated text in Korean
- * - Toast notifications for error handling
+ * - Alternating welcome messages in English and Korean
+ * - Rotating display of different mottos related to language learning
  * 
- * The component communicates with a backend server for translation through an API endpoint
- * ('http://localhost:8100/api/translate').
+ * The component uses React's useState and useEffect hooks for managing state and side effects
+ * It employs framer motion for animating the entrance and exit of text elements
  * 
- * The translation process is triggered by the 'handleTranslation' function, which sends
- * the user's input to the server and updates the translation state with the response
+ * Animation and State Management:
+ * - The welcome message and mottos are animated using Framer Motion to smoothly transition in and out
+ * - The useEffect hook is used to set intervals for alternating between English and Korean messages, changing mottos, and displaying different quotes
+ * - These intervals are cleared on component unmount to prevent memory leaks
  * 
- * Error Handling:
- * - In case of an error during the API request, a toast notification is displayed
- *   with an error message
  */
-function HomePage() {
-  const [textToTranslate, setTextToTranslate] = useState('');
-  const [translation, setTranslation] = useState('');
-  const toast = useToast();
 
-  const handleTranslation = async () => {
-    try {
-      const response = await axios.post('http://localhost:8100/api/translate', {
-        text: textToTranslate,
-        sourceLang: 'en',
-        targetLang: 'ko'
-      });
-      setTranslation(response.data.translation);
-    } catch (error) {
-      toast({
-        title: 'Error',
-        description: 'Failed to translate text.',
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  };
+function HomePage() {
+  const [showEnglish, setShowEnglish] = useState(true);
+  const [mottoIndex, setMottoIndex] = useState(0);
+  const [quoteIndex, setQuoteIndex] = useState(0);
+  
+  const mottos = [
+    "Master Korean, One Word at a Time",
+    "Embark on Your Korean Language Journey with Confidence",
+    "Learning Korean Made Fun and Interactive",
+  ];
+
+  const quotes = [
+    "Language is the road map of a culture. It tells you where its people come from and where they are going.",
+    "One language sets you in a corridor for life. Two languages open every door along the way.",
+    "Learning another language is not only learning different words for the same things but learning another way to think about things.",
+  ];
+
+  useEffect(() => {
+    const languageInterval = setInterval(() => {
+      setShowEnglish(prev => !prev);
+    }, 3000);
+
+    const mottoInterval = setInterval(() => {
+      setMottoIndex(prev => (prev + 1) % mottos.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(languageInterval);
+      clearInterval(mottoInterval);
+    };
+  }, []);
 
   return (
-    <Box p={5}>
-      <VStack spacing={4}>
-        <FormControl>
-          <FormLabel>English:</FormLabel>
-          <Textarea 
-            value={textToTranslate}
-            onChange={(e) => setTextToTranslate(e.target.value)}
-          />
-        </FormControl>
-        <Button colorScheme="blue" onClick={handleTranslation}>
-          Translate
-        </Button>
-        <FormControl>
-          <FormLabel>Korean:</FormLabel>
-          <Textarea isReadOnly value={translation} />
-        </FormControl>
-      </VStack>
+    <Box p={5} display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        key={showEnglish ? "english" : "korean"}
+      >
+        <Heading as="h1" size="4xl" textAlign="center" mb={4}>
+          {showEnglish ? "Welcome to Hanguru" : "한구루에 오신 것을 환영합니다"}
+        </Heading>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.8 }}
+        key={mottoIndex}
+      >
+        <Text fontSize="xl" textAlign="center" mb={3}>
+          {mottos[mottoIndex]}
+        </Text>
+      </motion.div>
+      <Text fontSize="md" textAlign="center" maxWidth="600px">
+        {quotes[quoteIndex]}
+      </Text>
     </Box>
   );
 }
