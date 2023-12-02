cdb71b489e3f5ac77a746dda2f070a5d6e5c1c3f
Glendon Chin <glendonchin@gmail.com>
2023-12-02 18:50:20 -0500

    log session details

--- server/app.js
+++ server/app.js
@@ -50,6 +50,7 @@ app.use(passport.session());
 app.use((req, res, next) => {
   console.log('Session ID:', req.sessionID);
   console.log('Session User:', req.user);
+  console.log('Session Details:', req.session);
   next();
 });
 
