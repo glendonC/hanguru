13d249e3aa0ace67b5ff672e4334711f5c6b7638
Glendon Chin <glendonchin@gmail.com>
2023-12-08 10:10:58 -0500

    update auth doc

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,21 +5,20 @@ const passport = require('passport');
 const User = require('../models/User');
 
 /**
- * POST /register
- * This endpoint handles user registration. It receives user data, encrypts the password, 
- * and saves the new user to the database
- * 
- * Request body:
- * - username: The username of the new user
- * - email: The email address of the new user
- * - password: The password of the new user
- * 
- * Response:
- * - On success: 201 status with a message 'User created' and user details (username and email)
- * - On error: 400 status with a message 'Cannot register user'
- * 
- * The password is hashed using bcrypt before storing it in the database
- */
+  * POST /register
+  * Handles user registration. It receives user data, encrypts the password using bcrypt, 
+  * and saves the new user to the database.
+  * 
+  * Request body:
+  * - username: The username of the new user.
+  * - email: The email address of the new user.
+  * - password: The password of the new user.
+  * 
+  * Response:
+  * - On success: 201 status with message 'User created' and user details (username and email).
+  * - On error: 400 status with message 'Cannot register user'.
+  * Note: Password is hashed using bcrypt before storing in the database for security.
+*/
 router.post('/register', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -38,20 +37,19 @@ router.post('/register', async (req, res) => {
 
 
 /**
- * POST /login
- * This endpoint handles user login using Passport's local strategy
- * It authenticates the user based on the provided username and password
- * 
- * Request body:
- * - Contains the username and password entered by the user
- * 
- * Response:
- * - On successful authentication: The user object with a message 'Successfully Authenticated'
- * - On failure: 401 status with a message 'No User Exists or Password Incorrect'
- * 
- * Uses Passport for authentication. Passport utilizes a 'local' strategy to validate the user credentials
- * In case of errors during the login process, appropriate messages are returned to the client
- */
+  * POST /login
+  * Handles user login using Passport's local strategy. Authenticates the user based on username and password.
+  * 
+  * Request body:
+  * - username: The user's username.
+  * - password: The user's password.
+  * 
+  * Response:
+  * - On success: Returns the user object with message 'Successfully Authenticated'.
+  * - On failure: 401 status with message 'No User Exists or Password Incorrect'.
+  * 
+  * Note: Utilizes Passport for authentication with a 'local' strategy.
+*/
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
@@ -61,8 +59,6 @@ router.post('/login', (req, res, next) => {
     if (!user) {
       return res.status(401).json({ message: 'No User Exists or Password Incorrect' });
     }
-
-    console.log("User found in login route: ", user);
 
     req.logIn(user, async (err) => {
       console.log('User logged in, Session ID:', req.sessionID);
@@ -77,11 +73,7 @@ router.post('/login', (req, res, next) => {
         sameSite: 'None',
         maxAge: 24 * 60 * 60 * 1000
       });
-      
-
-
-      console.log("User logged in: ", req.user);
-
+  
       const currentDate = new Date().toISOString();
       if (!user.loginDates.some((date) => date.toISOString().split('T')[0] === currentDate.split('T')[0])) {
         user.loginDates.push(currentDate);
@@ -93,15 +85,19 @@ router.post('/login', (req, res, next) => {
           throw error;
         }
       }
-
       const streak = calculateStreak(user.loginDates);
-
       res.json({ message: 'Successfully Authenticated', user: user, streak: streak });
     });
   })(req, res, next);
 });
 
-
+/**
+  * calculateStreak
+  * Calculates the user's login streak based on their login dates.
+  * 
+  * @param {Array} dates - Array of user's login dates.
+  * @returns {number} The calculated login streak.
+*/
 function calculateStreak(dates) {
   let streak = 0;
   let currentDate = new Date();
@@ -119,8 +115,16 @@ function calculateStreak(dates) {
   return streak;
 }
 
-// POST /signout
-// This endpoint handles user sign-out
+/**
+  * POST /logout
+  * Handles user logout. Ends the user's session and sends a confirmation response.
+  * 
+  * Response:
+  * - On success: 200 status with message 'Logged out'.
+  * - On error: Returns error if logout process fails.
+  * 
+  * Note: Utilizes Passport's req.logout function to end the user session.
+*/
 router.post('/logout', (req, res) => {
   req.logout(function(err) {
     if (err) { 
