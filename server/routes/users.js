3778865540c688ad12551488316f8bd9210b9785
Glendon Chin <glendonchin@gmail.com>
2023-12-08 10:17:21 -0500

    update users route doc

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const path = require('path');
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
+const User = require('../models/User');
+const ProfilePicture = require('../models/ProfilePicture');
 
+/**
+ * Function to generate a signed URL for accessing a stored image
+ * @param {string} imageUrl The URL of the image stored in Google Cloud Storage
+ * @returns {Promise<string>} A promise that resolves to a signed URL of the image
+*/
 async function generateSignedUrl(imageUrl) {
   const bucketName = 'hanguru-profile-pic-bucket';
   const fileName = imageUrl.split('/').pop();
@@ -21,11 +28,14 @@ async function generateSignedUrl(imageUrl) {
     throw error;
   }
 }
-
-
-const User = require('../models/User');
-const ProfilePicture = require('../models/ProfilePicture');
-
+/**
+ * GET /get-users
+ * Retrieves all user data from the database.
+ * 
+ * Response:
+ * - On success: JSON array of all users.
+ * - On error: 500 status with error message 'Internal Server Error'.
+*/
 router.get('/get-users', async (req, res) => {
   try {
     const users = await User.find({});
@@ -36,6 +46,18 @@ router.get('/get-users', async (req, res) => {
   }
 });
 
+/**
+ * GET /profile-pictures/:id
+ * Retrieves a specific profile picture by its ID and generates a signed URL for it.
+ * 
+ * Request parameters:
+ * - id: ID of the profile picture to retrieve.
+ * 
+ * Response:
+ * - On success: JSON object with the signed URL of the image.
+ * - On not found: 404 status with error message 'Profile picture not found'.
+ * - On error: 500 status with error message 'Internal Server Error'.
+*/
 router.get('/profile-pictures/:id', async (req, res) => {
   const pictureId = req.params.id;
 
@@ -53,7 +75,14 @@ router.get('/profile-pictures/:id', async (req, res) => {
   }
 });
 
-
+/**
+ * GET /profile-pictures
+ * Retrieves all profile pictures from the database.
+ * 
+ * Response:
+ * - On success: JSON array of all profile pictures.
+ * - On error: 500 status with error message 'Internal Server Error'.
+*/
 router.get('/profile-pictures', async (req, res) => {
   try {
     console.log('Fetching profile pictures...');
@@ -71,10 +100,18 @@ router.get('/profile-pictures', async (req, res) => {
   }
 });
 
-
-
-
-
+/**
+ * POST /upload
+ * Saves a profile picture's metadata in the database.
+ * 
+ * Request body:
+ * - userId: ID of the user to which the profile picture belongs.
+ * - imageUrl: URL of the uploaded profile picture.
+ * 
+ * Response:
+ * - On success: 200 status with success message 'Profile picture saved successfully'.
+ * - On error: 500 status with error message.
+*/
 router.post('/upload', async (req, res) => {
   const { userId, imageUrl } = req.body;
 
@@ -86,6 +123,18 @@ router.post('/upload', async (req, res) => {
   }
 });
 
+/**
+ * GET /:userId
+ * Retrieves the profile picture for a specific user.
+ * 
+ * Request parameters:
+ * - userId: ID of the user whose profile picture is to be retrieved.
+ * 
+ * Response:
+ * - On success: JSON object with the URL of the user's profile picture.
+ * - On not found: 404 status with error message 'Profile picture not found'.
+ * - On error: 500 status with error message 'Error fetching profile picture'.
+*/
 router.get('/:userId', async (req, res) => {
   const userId = req.params.userId;
 
@@ -102,6 +151,17 @@ router.get('/:userId', async (req, res) => {
   }
 });
 
+/**
+ * GET /user/:userId/login-streak
+ * Calculates and returns the login streak of a specific user.
+ * 
+ * Request parameters:
+ * - userId: ID of the user whose login streak is to be calculated.
+ * 
+ * Response:
+ * - On success: JSON object with the user's login streak.
+ * - On error: 500 status with error message.
+*/
 router.get('/user/:userId/login-streak', (req, res) => {
   User.findById(req.params.userId, (err, user) => {
       if (err) {
@@ -116,6 +176,11 @@ router.get('/user/:userId/login-streak', (req, res) => {
   });
 });
 
+/**
+ * Function to calculate the login streak from an array of login dates
+ * @param {Array<Date>} dates Array of user's login dates
+ * @returns {number} The calculated login streak
+*/
 function calculateStreak(dates) {
   let streak = 0;
   let currentDate = new Date();
@@ -133,6 +198,14 @@ function calculateStreak(dates) {
   return streak;
 }
 
+/**
+ * GET /current-user
+ * Retrieves information about the currently authenticated user.
+ * 
+ * Response:
+ * - If authenticated: JSON object with the current user's data.
+ * - If not authenticated: 401 status with message 'Not authenticated'.
+*/
 router.get('/current-user', (req, res) => {
   console.log('Received request to /api/users/current-user');
   if (req.isAuthenticated()) {
@@ -141,8 +214,5 @@ router.get('/current-user', (req, res) => {
     res.status(401).json({ message: 'Not authenticated' });
   }
 });
-
-
-
 
 module.exports = router;
