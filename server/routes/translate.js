2d8e8bb84a5c674a6cb50936eb9c33f499525855
Glendon Chin <glendonchin@gmail.com>
2023-12-08 10:11:37 -0500

    update translate route doc

--- server/routes/translate.js
+++ server/routes/translate.js
@@ -5,21 +5,20 @@ const googleTranslate = new GoogleTranslateWrapper(process.env.GOOGLE_API_KEY);
 
 /**
  * POST /translate
- * This endpoint handles requests for text translation
- * It utilizes the GoogleTranslateWrapper to interface with the Google Translate API
+ * Handles text translation requests using the Google Translate API.
  * 
  * Request body:
- * - text: The text to be translated
- * - sourceLang: The language code of the source text
- * - targetLang: The language code of the target translation
+ * - text: String representing the text to be translated.
+ * - sourceLang: Language code of the source text.
+ * - targetLang: Language code of the target translation.
  * 
  * Response:
- * - On success: Returns a JSON object containing the 'translation' of the provided text
- * - On error: Returns a 500 status with an error message
+ * - On success: Returns a JSON object containing the 'translation' of the provided text.
+ * - On error: Returns a 500 status with an error message.
  * 
- * The translation is done by calling the 'translate' method of the GoogleTranslateWrapper instance
- * This method communicates with the Google Translate API to get the translated text
- */
+ * Utilizes 'GoogleTranslateWrapper' to interface with the Google Translate API.
+ * The 'translate' method of the GoogleTranslateWrapper instance is called to perform the translation.
+*/
 router.post('/translate', async (req, res) => {
     const { text, sourceLang, targetLang } = req.body;
     console.log(req.body);
